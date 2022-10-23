8397f20429eebc67a07278f52c5f3cf5fe10a3da
ThomasDev-de <67106837+thomasdev-de@users.noreply.github.com>
2022-10-23 22:39:51 +0200

    test 2

--- demo/actions.php
+++ demo/actions.php
@@ -1,14 +1,37 @@
 <?php
+$countries = json_decode(file_get_contents('countries.json'));
 
-$items = [];
+$value = filter_input(INPUT_GET, 'value', FILTER_VALIDATE_INT);
+$limit = filter_input(INPUT_GET, 'limit', FILTER_VALIDATE_INT) || 10;
+$q = filter_input(INPUT_GET, 'q');
+$search = empty($q)? false : strtolower($q);
+$return = [];
+
+$fetchSingleDate = ! empty($value);
+if ($fetchSingleDate)
+{
+	$data = array_filter($countries, static function($country) use ($value){
+		return $country->id === $value;
+	});
+	$return['selectedItem'] = $data[0];
+}
+else
+{
+	$data = $search === false ? [] :  array_slice(
+		array:array_filter($countries, static function($country) use ($search){
+			return str_contains(strtolower($country->text), $search);
+		}),
+		offset: 0,
+		length: $limit
+	);
+	$return['items'] = $data;
+	$return['total'] = count($countries);
+}
+
+http_response_code(200);
+header('Content-Type: application/json');
 try {
-	$item = new stdClass();
-	$item->text = "John Doe";
-	$item->id = 1;
-	$items[] = $item;
-	http_response_code(200);
-	header('Content-Type: application/json');
-	exit(json_encode($items, JSON_THROW_ON_ERROR));
+	exit(json_encode($return, JSON_THROW_ON_ERROR));
 } catch (JsonException $e) {
 	http_response_code(500);
 	exit($e->getMessage());
