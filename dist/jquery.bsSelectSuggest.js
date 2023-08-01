8064ece435ea96f95ea38663d849793cd66b9c92
thomas <t.kirsch@webcito.de>
2023-08-01 08:03:06 +0200

    .

--- dist/jquery.bsSelectSuggest.js
+++ dist/jquery.bsSelectSuggest.js
@@ -3,23 +3,6 @@
 /** global $ */
 (function ($) {
     const debug = false;
-    const DEFAULTS = {
-        limit: 10,
-        typingInterval: 400,
-        multiple: false,
-        valueSeparator: ',',
-        darkMenu: false,
-        btnWidth: 'fit-content',
-        btnClass: 'btn btn-outline-secondary',
-        searchPlaceholderText: "Search",
-        emptyText: 'Please choose..',
-        waitingForTypingText: 'Waiting for typing',
-        typingText: 'typing..',
-        loadingText: 'Loading..',
-        queryParams: function (params) {
-            return params;
-        }
-    };
 
     function generateId() {
         return "webcito_suggestion_" + getGUID();
@@ -29,10 +12,10 @@
         let settings = select.data('settings');
         let darkClass = settings.darkMenu ? 'dropdown-menu-dark' : '';
         let closeBtnClass = settings.darkMenu ? 'btn-close-white' : '';
-        return `
+        const template = `
             <div class="dropdown">
                   <div class="${settings.btnClass} d-flex align-items-center" data-bs-toggle="dropdown" aria-expanded="false" style="width:${settings.btnWidth}">
-                    <span class="js-selected-text"></span>
+                        <span class="js-selected-text">${settings.emptyText}</span>
                   </div>
                   <div class="dropdown-menu ${darkClass} p-0 mt-1">
                     <div class="card bg-transparent border-0 m-0 w-100">
@@ -49,6 +32,11 @@
                     </div>
                   </div>
             </div>`;
+
+        if (debug) {
+            console.log(template);
+        }
+        return template;
     }
 
     function getWrapper(select) {
@@ -58,21 +46,22 @@
     function buildDropdown(select) {
 
         let w = getWrapper(select);
-        if (w.length) {
+        if (w.length === 1) {
             return w;
         }
 
-        let id = generateId();
-        let wrap = $('<div>', {
+        const id = generateId();
+        const wrap = $('<div>', {
             id: id
         }).insertAfter(select);
         select.hide();
         select.appendTo(wrap);
-        $(getTemplate(select)).insertBefore(select);
+        const template = getTemplate(select);
+        $(template).prependTo(wrap);
         // setTimeout(function () {
-            if (wrap.find('.js-selected-text').text() === "") {
-                setDropdownText(select,null );
-            }
+        if (wrap.find('.js-selected-text').text() === "") {
+            setDropdownText(select, null);
+        }
         // }, 40);
         return wrap;
 
@@ -99,14 +88,17 @@
     }
 
     function setDropdownText(select, html) {
-        let settings = select.data('settings');
-        // alert(select.prop('name'));
-        getWrapper(select).find('.js-selected-text').html('<span class="px-2 py-1 d-inline">' +
-            (html || settings.emptyText) + '</span>');
+
+        const wrapper = getWrapper(select);
+        const settings = select.data('settings');
+        if (debug) {
+            console.log('setDropdownText', html, wrapper, settings);
+        }
+        wrapper.find('.js-selected-text').html('<span class="px-2 py-1 d-inline">' + (html || settings.emptyText) + '</span>');
     }
 
     function getGUID() {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         );
     }
@@ -124,12 +116,12 @@
     }
 
     function setStatus(select, text) {
-        const wrapper = getWrapper(select),
-            statusBox = wrapper.find('.suggest-status-text');
+        const wrapper = getWrapper(select);
+        const statusBox = wrapper.find('.suggest-status-text');
         statusBox.html(text);
     }
 
-    function getSettings(select){
+    function getSettings(select) {
         return select.data('settings') || {};
     }
 
@@ -143,6 +135,9 @@
         const list = wrapper.find('.card-body');
 
         searchBox.on('keyup', function () {
+            if (debug) {
+                console.log('keyup');
+            }
             if (typingTimer !== null) {
                 clearTimeout(typingTimer);
             }
@@ -155,6 +150,9 @@
         });
 
         searchBox.on('keydown', function () {
+            if (debug) {
+                console.log('keydown');
+            }
             let settings = getSettings(select);
             if (typingTimer !== null) {
                 clearTimeout(typingTimer);
@@ -166,6 +164,9 @@
         wrapper
             .on('click', 'a.dropdown-item', function (e) {
                 e.preventDefault();
+                if (debug) {
+                    console.log('click', 'a.dropdown-item');
+                }
                 let a = $(e.currentTarget);
                 let item = a.data('item');
                 select.trigger('change', [item.id, item.text]);
@@ -176,6 +177,9 @@
             })
             .on('click', '.js-webcito-reset', function (e) {
                 e.preventDefault();
+                if (debug) {
+                    console.log('click', '.js-webcito-reset');
+                }
                 // reset(select);
                 select.val(null);
                 searchBox.val(null);
@@ -185,12 +189,18 @@
                 setStatus(select, settings.waitingForTypingText);
             })
             .on('hidden.bs.dropdown', '.dropdown', function () {
+                if (debug) {
+                    console.log('hidden.bs.dropdown', '.dropdown');
+                }
                 list.empty();
                 searchBox.val(null);
                 let settings = getSettings(select);
                 setStatus(select, settings.waitingForTypingText);
             })
             .on('shown.bs.dropdown', '.dropdown', function () {
+                if (debug) {
+                    console.log('shown.bs.dropdown', '.dropdown');
+                }
                 searchBox.focus();
             });
     }
@@ -238,7 +248,7 @@
 
     $.fn.suggest = function (options, params) {
 
-        if (! $(this).length) {
+        if (!$(this).length) {
             return $(this); // cancel
         }
 
@@ -248,22 +258,45 @@
             })
         }
 
+        const DEFAULTS = {
+            limit: 10,
+            typingInterval: 400,
+            multiple: false,
+            valueSeparator: ',',
+            darkMenu: false,
+            btnWidth: 'fit-content',
+            btnClass: 'btn btn-outline-secondary',
+            searchPlaceholderText: "Search",
+            emptyText: 'Please choose..',
+            waitingForTypingText: 'Waiting for typing',
+            typingText: 'typing..',
+            loadingText: 'Loading..',
+            queryParams: function (params) {
+                return params;
+            }
+        };
+
         const select = $(this); // The single instance
         const isOptionsSet = typeof options === "object" || typeof options === "undefined";
         const isCallMethod = typeof options === "string";
 
         // init
         if (select.data('initSuggest') !== true) {
+
             select.data('initSuggest', true);
             select.addClass('js-suggest');
 
-            if (isOptionsSet || ! select.data('settings')) {
+            if (isOptionsSet || !select.data('settings')) {
                 const settings = $.extend(true, DEFAULTS, options || {});
                 select.data('settings', settings);
                 select.data('selected', select.val().split(settings.valueSeparator));
+                if (debug) {
+                    console.log('init', select, settings);
+                }
             }
 
             buildDropdown(select);
+
             events(select);
 
             if (select.val() !== "") {
@@ -275,17 +308,30 @@
         if (isCallMethod) {
             switch (options.toLowerCase()) {
                 case 'val':
+                    if (debug) {
+                        console.log('method', 'val', params, select);
+                    }
                     reset(select);
                     getData(select, false, params);
                     break;
                 case 'destroy':
+                    if (debug) {
+                        console.log('method', 'destroy', select);
+                    }
                     destroy(select, true);
                     break;
                 case 'refresh':
+                    if (debug) {
+                        console.log('method', 'refresh', select);
+                    }
                     refresh(select);
                     break;
                 case 'updateoptions': {
-                    select.data('settings', $.extend(true, getSettings(select), params || {}, DEFAULTS));
+                    if (debug) {
+                        console.log('method', 'updateoptions', params, select);
+                    }
+                    const oldSettings = getSettings(select);
+                    select.data('settings', $.extend(true, DEFAULTS, oldSettings, params || {}));
                     refresh(select);
                     break;
                 }
